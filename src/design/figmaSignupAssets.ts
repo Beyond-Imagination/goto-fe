323a242fc4495b625edf0d92468b3c5d22c4cc48
joonamin <joonamin44@gmail.com>
2026-08-14 20:30:29 +0900

    fix(design): 약관 동의 화면 쉐브론 벡터 아이콘 추가 및 에셋 등록

--- src/design/figmaSignupAssets.ts
+++ src/design/figmaSignupAssets.ts
@@ -7,6 +7,7 @@ function resolveAssetUri(asset: number): string {
 /** 새 가입 화면에 사용하는 Figma 원본 벡터 에셋. */
 export const FIGMA_SIGNUP_ASSETS = {
   arrowRight: resolveAssetUri(require('@/assets/signup/arrow-right.svg')),
+  chevronRight: resolveAssetUri(require('@/assets/signup/chevron-right.svg')),
   checkDisabledBackground: resolveAssetUri(require('@/assets/signup/check-disabled-background.svg')),
   checkSelectedBackground: resolveAssetUri(require('@/assets/signup/check-selected-background.svg')),
   defaultAvatarAdd: resolveAssetUri(require('@/assets/signup/default-avatar-add.svg')),
